ba9c7639cabb3e7c4b2012a568ab8335edb58f5c
alishba200 <153546780+alishba200@users.noreply.github.com>
2024-08-15 20:22:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//Q1
+var myage=20;
+alert("I am "+myage+" years old")
+//Q2
+var mynum;
+mynum=5;
+alert("You have visited this site "+mynum+" times <br>")
+//Q3
+var birthYear=2004;
+document.write(" <br><br>My birth year is "+birthYear+"<br>") 
+document.write("Datatype of my declared variable is number. <br><br>")
+//Q4
+var visitorName="John Doe";
+var productTitle="T-shirt";
+var quantity=5;
+document.write("<b>"+visitorName+"</b> ordered <b>"+quantity+"</b>  <b>"+ productTitle+"</b>(s) on XYZ Clothing store.")
